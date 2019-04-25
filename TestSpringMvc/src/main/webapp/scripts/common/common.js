7422234918293f8a5e58974c30d182f198ba26dc
xiaoxia <1101788591@qq.com>
2019-04-25 10:41:55 +0800

    引入日期js

--- TestSpringMvc/src/main/webapp/scripts/common/common.js
+++ TestSpringMvc/src/main/webapp/scripts/common/common.js
@@ -66,28 +66,3 @@ function addTabs(title, url){
         });
     }
 }
-
-function ww3(date){
-    var y = date.getFullYear();
-    var m = date.getMonth()+1;
-    var d = date.getDate();
-    var h = date.getHours();
-    var mi = date.getMinutes(date);
-    var s = date.getSeconds(date);
-    var str = y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d)+' '+(h<10?('0'+h):h+':'+(mi<10?('0'+mi):mi)+":"+(s<10?('0'+s):s));
-    return str;
-}
-function w3(s){
-    if (!s) return new Date();
-    var y = s.substring(0,4);
-    var m =s.substring(5,7);
-    var d = s.substring(8,10);
-    var h = s.substring(11,14);
-    var min = s.substring(15,17);
-    var sec = s.substring(18,20);
-    if (!isNaN(y) && !isNaN(m) && !isNaN(d) && !isNaN(h) && !isNaN(min) && !isNaN(sec)){
-        return new Date(y,m-1,d,h,min,sec);
-    } else {
-        return new Date();
-    }
-}
